696435a8e3fd9f7c70a4563dbec9fdc2c21f2f3b
Ahmad Ajmal <ahmadajmal9301@gmail.com>
2022-09-05 12:41:21 +0500

    changed images

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,42 +1,25 @@
-import self from "../img/self.png"
-import temp1 from '../img/temp1.jpg'
-import temp2 from '../img/temp2.jpg'
-import temp3 from '../img/temp3.jpg'
-import temp4 from '../img/temp4.jpg'
-import temp5 from '../img/temp5.jpg'
-import temp6 from '../img/temp6.jpg'
-import temp7 from '../img/temp7.jpg'
-import temp8 from '../img/temp8.jpg'
-import temp9 from '../img/temp9.jpg'
-import temp10 from '../img/temp10.jpg'
+import self from "../img/AhmadAjmal.jpg"
+import temp4 from '../img/temp4.png'
+import temp5 from '../img/temp5.png'
+import temp6 from '../img/temp6.png'
+import temp7 from '../img/temp7.png'
+import temp8 from '../img/temp8.png'
+import temp9 from '../img/temp9.png'
+import temp10 from '../img/temp10.png'
+import temp11 from '../img/temp11.png'
 import mock5 from "../img/mock5.png"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
     firstName: "Ahmad",
     lastName: "Ajmal",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "js", 
     position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self, 
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [ 
         {
             emoji: '☕',
             text: 'fueled by coffee'
@@ -80,8 +63,6 @@ export const info = {
         //     icon: "fa fa-twitter",
         //     label: 'twitter'
         // }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
 
     ],
     bio: "Hello! I'm Ahmad. I'm a Full Stack Freelance Developer. I studied CompSci at FAST NUCES, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
@@ -108,15 +89,13 @@ export const info = {
             label: 'Movies',
             emoji: '🎥'
         },
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [ 
         {
             title: "Rastay.pk",
-            live: "http://rastay.pk/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/PeachyVVolf", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: temp1
+            live: "http://rastay.pk/", 
+            source: "https://github.com/PeachyVVolf", 
+            image: temp11
         },
         {
             title: "Fulminare",
@@ -130,12 +109,12 @@ export const info = {
             source: "https://github.com/PeachyVVolf/reachoutkids",
             image: temp4
         },
-        {
-            title: "Netflix Copy",
-            live: "",
-            source: "",
-            image: mock5
-        },
+        // {
+        //     title: "Netflix Copy",
+        //     live: "",
+        //     source: "",
+        //     image: mock5
+        // },
         {
             title: "React Colors",
             live: "http://fulminare.me/react-colors/",
@@ -159,18 +138,6 @@ export const info = {
             live: "http://fulminare.me/dadjokes-app/",
             source: "https://github.com/PeachyVVolf/dadjokes-app",
             image: temp10
-        },
-        {
-            title: "Stradale Coffee",
-            live: "",
-            source: "",
-            image: temp2
-        },
-        {
-            title: "MaxShop",
-            live: "",
-            source: "",
-            image: temp3
         },
         {
             title: "Blog App",
